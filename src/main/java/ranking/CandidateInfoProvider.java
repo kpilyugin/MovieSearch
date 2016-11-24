27e264a19f553fa2223cb2a33f74b11731ff4266
Evgeniy Akimov <geka666@gmail.com>
2016-11-25 00:33:36 +0300

    db cache

--- src/main/java/ranking/CandidateInfoProvider.java
+++ src/main/java/ranking/CandidateInfoProvider.java
@@ -1,11 +1,12 @@
 package ranking;
 
+import lombok.Data;
 import ranking.Ranking.SearchCandidate;
 import search.DbConnection;
 
-import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashMap;
 
 public class CandidateInfoProvider implements AutoCloseable {
     private final DbConnection conn;
@@ -20,36 +21,93 @@ public class CandidateInfoProvider implements AutoCloseable {
     }
 
     public SearchCandidate getInfo(String movieId, double luceneScore) throws SQLException {
-        conn.movieRequestStatement.setString(1, movieId);
-        final ResultSet movieInfo = conn.movieRequestStatement.executeQuery();
-        if (!movieInfo.next()) {
-            System.err.println("movieInfo not found!");
-        }
-
-        conn.searchRequestStatement.setString(1, movieId);
-        final ResultSet searchInfo = conn.searchRequestStatement.executeQuery();
-        if (!searchInfo.next()) {
-            System.err.println("searchInfo not found!");
-        }
-
-        int reviewCount = 0;
-        PreparedStatement s = conn.connection.prepareStatement("SELECT cnt FROM review_counts WHERE movie_id = ?");
-        s.setString(1, movieId);
-        final ResultSet reviewCntRes = s.executeQuery();
-        if (reviewCntRes.next()) {
-            reviewCount = reviewCntRes.getInt("cnt");
-        }
+        final MovieInfo movieInfo = getMovie(movieId);
+        final SearchInfo searchInfo = getSearch(movieId);
 
         return new SearchCandidate(
                 movieId,
-                movieInfo.getString("title"),
-                movieInfo.getString("date"),
-                movieInfo.getString("poster_url"),
-                searchInfo.getString("plot"),
-                searchInfo.getString("reviews"),
-                searchInfo.getString("preprocessed_plot"),
+                movieInfo.getTitle(),
+                movieInfo.getDate(),
+                movieInfo.getPoster_url(),
+                searchInfo.getPlot(),
+                searchInfo.getReviews(),
+                searchInfo.getPreprocessed_plot(),
                 luceneScore,
-                reviewCount
+                movieInfo.getReviewsCount()
         );
+    }
+
+    private MovieInfo getMovie(String movieId) throws SQLException {
+        if (movieCache.containsKey(movieId)) {
+            return movieCache.get(movieId);
+        }
+        conn.movieRequestStatement.setString(1, movieId);
+        final ResultSet movieResults = conn.movieRequestStatement.executeQuery();
+        if (!movieResults.next()) {
+            System.err.println("movieInfo not found!");
+        }
+        conn.reviewsCountStatement.setString(1, movieId);
+        final ResultSet reviewCntResults = conn.reviewsCountStatement.executeQuery();
+        if (!reviewCntResults.next()) {
+            System.err.println("review counts not found!");
+        }
+        final MovieInfo movieInfo = new MovieInfo(movieResults, reviewCntResults);
+        movieCache.put(movieId, movieInfo);
+        return movieInfo;
+    }
+
+    private SearchInfo getSearch(String movieId) throws SQLException {
+        if (searchCache.containsKey(movieId)) {
+            return searchCache.get(movieId);
+        }
+        conn.searchRequestStatement.setString(1, movieId);
+        final ResultSet searchResults = conn.searchRequestStatement.executeQuery();
+        if (!searchResults.next()) {
+            System.err.println("searchInfo not found!");
+        }
+        final SearchInfo searchInfo = new SearchInfo(searchResults);
+        searchCache.put(movieId, searchInfo);
+        return searchInfo;
+    }
+
+    private static final HashMap<String, MovieInfo> movieCache = new HashMap<>();
+    private static final HashMap<String, SearchInfo> searchCache = new HashMap<>();
+
+    @Data
+    private static class MovieInfo {
+        private final String imdb_id;
+        private final String title;
+        private final String plot;
+        private final String poster_url;
+        private final String actors;
+        private final String date;
+        private final int reviewsCount;
+
+        public MovieInfo(ResultSet movieResults, ResultSet reviewsResult) throws SQLException {
+            this.imdb_id = movieResults.getString("imdb_id");
+            this.title = movieResults.getString("title");
+            this.plot = movieResults.getString("plot");
+            this.poster_url = movieResults.getString("poster_url");
+            this.actors = movieResults.getString("actors");
+            this.date = movieResults.getString("date");
+            this.reviewsCount = reviewsResult.getInt("cnt");
+        }
+    }
+
+    @Data
+    private static class SearchInfo {
+        private final String movie_id;
+        private final String plot;
+        private final String reviews;
+        private final String preprocessed_plot;
+        private final String preprocessed_reviews;
+
+        public SearchInfo(ResultSet resultSet) throws SQLException {
+            this.movie_id = resultSet.getString("movie_id");
+            this.plot = resultSet.getString("plot");
+            this.reviews = resultSet.getString("reviews");
+            this.preprocessed_plot = resultSet.getString("preprocessed_plot");
+            this.preprocessed_reviews = resultSet.getString("preprocessed_reviews");
+        }
     }
 }
--- src/main/java/search/DbConnection.java
+++ src/main/java/search/DbConnection.java
@@ -10,17 +10,20 @@ public class DbConnection implements AutoCloseable {
     private static final String DATABASE = "jdbc:postgresql://localhost:5432/imdbcrawl";
     private static final String movieQuery = "SELECT * FROM movie WHERE imdb_id = ?";
     private static final String searchQuery = "SELECT * FROM search WHERE movie_id = ?";
+    private static final String reviewsCountQuery = "SELECT cnt FROM review_counts WHERE movie_id = ?";
     //private static final String allSearch = "SELECT * FROM search";
 
     public final Connection connection;
     public final PreparedStatement movieRequestStatement;
     public final PreparedStatement searchRequestStatement;
+    public final PreparedStatement reviewsCountStatement;
     //public final PreparedStatement searchAllStatement;
 
     public DbConnection() throws SQLException {
         connection = DriverManager.getConnection(DATABASE, "ir", "");
         movieRequestStatement = connection.prepareStatement(movieQuery);
         searchRequestStatement = connection.prepareStatement(searchQuery);
+        reviewsCountStatement = connection.prepareStatement(reviewsCountQuery);
         //searchAllStatement = connection.prepareStatement(allSearch);
     }
 
@@ -28,6 +31,7 @@ public class DbConnection implements AutoCloseable {
     public void close() throws SQLException {
         movieRequestStatement.close();
         searchRequestStatement.close();
+        reviewsCountStatement.close();
         connection.close();
     }
 }
